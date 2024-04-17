76099469a1db42004761a422548ece4e92d48bbd
maamirkh <maamirkhan1983@gmail.com>
2024-04-18 01:57:35 +0500

    updated

---
+++ app.js
@@ -0,0 +1,8 @@
+const students = new Map();
+students.set(1, "Alice");
+students.set(2, "Bob");
+students.set(3, "Charlie");
+students.forEach((name, id) => {
+    console.log(`Student ID: ${id}, Name: ${name}`);
+});
+export {};
